3fb9645ccf7e6abb44245e05bf8b83a75b7af01d
Manan Gupta <guptamanan576@gmail.com>
2025-09-24 08:59:45 +0530

    done

--- axios.js
+++ axios.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://api.render.com/deploy/srv-d39m3qje5dus73bitoeg?key=3PJqY7aFozo/api/v1", // adjust for production later
+  baseURL: "https://alumnet-s.onrender.com/api/v1", // adjust for production later
   withCredentials: true, // if using cookies/auth
 });
 
